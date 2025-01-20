31cd73eb55b678f80b6ebd6a8df8fd4fe9e40b8e
T3thr <t.theerapat33@gmail.com>
2025-01-20 07:00:40 +0700

    keyof

--- components/Student.tsx
+++ components/Student.tsx
@@ -96,7 +96,7 @@ export default function Student({ students }: StudentProps) {
                 <td className="p-4">{student.id}</td>
                 {months.map(({ name, year }) => (
                   <td key={`${student.id}-${name}`} className="p-4 text-center">
-                    {formatPaymentStatus(student[name.toLowerCase().slice(0, 3)], student.id, name, year, parseInt(student.no))}
+                    {formatPaymentStatus(student[name.toLowerCase().slice(0, 3) as keyof StudentData], student.id, name, year, parseInt(student.no))}
                   </td>
                 ))}
                 <td className="p-4 text-left">{student.note}</td>
