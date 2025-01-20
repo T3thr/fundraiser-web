ceeff274f31901127a692df4603610cd66c56bc2
T3thr <t.theerapat33@gmail.com>
2025-01-21 00:19:36 +0700

    delete page

--- backend/lib/constants.ts
+++ backend/lib/constants.ts
@@ -1,6 +1,6 @@
 // @/backend/lib/constants.ts
 export const PAYMENT_CONFIGS = {
-    CURRENT_MONTH_FEE: 70,
+    CURRENT_MONTH_FEE: 10,
     LATE_PAYMENT_FEE: 80,
     CURRENCY: 'thb',
     SUCCESS_URL: `${process.env.NEXT_PUBLIC_BASE_URL}/payment/success`,
